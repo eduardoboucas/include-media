33d12f9fd7d726995f1249a77284dd71b6fe5e4a
Eduardo Bouças <mail@eduardoboucas.com>
2014-12-29 19:16:55 +0000

    Change name

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function(grunt) {
 			},
 			dist: {
 	  			src: ['src/banner.md', 'src/_config.scss', 'src/_media.scss', 'src/helpers/*.scss', 'src/vendor/*.scss'],
-	  			dest: 'dist/_import-media.scss',
+	  			dest: 'dist/_include-media.scss',
 			},
 		},
 
