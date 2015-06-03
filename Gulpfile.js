b86918b1271c5a7e93d9ddcf9f7a8fb7dde257b9
Hugo Giraudel <hugo.giraudel@gmail.com>
2015-06-03 22:56:01 +0200

    Cleaned things up a bit

--- Gulpfile.js
+++ Gulpfile.js
@@ -10,10 +10,6 @@ var sassInput = [
   'src/_media.scss'
 ];
 
-var sassOptions = {
-  errLogToConsole: true
-};
-
 
 // -----------------------------------------------------------------------------
 // Dependencies
@@ -23,13 +19,12 @@ var fs = require('fs');
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 var packageInfo = require('./package.json');
-var path = require('path');
 
 // -----------------------------------------------------------------------------
 // Dist
 // -----------------------------------------------------------------------------
 
-gulp.task('concat', function () {
+gulp.task('build', function () {
   return gulp
     .src(sassInput)
     .pipe(plugins.concat('_include-media.scss'))
@@ -45,17 +40,17 @@ gulp.task('concat', function () {
 
 gulp.task('test_libsass', function () {
   return gulp
-    // Run all tests in `tests` folder with LibSass
     .src(['./tests/*.scss'])
-    .pipe(plugins.sass(sassOptions).on('error', function (err) {
-      plugins.sass.logError(err);
-      process.exit(1);
-    }));
+    .pipe(plugins.sass({ errLogToConsole: true })
+      .on('error', function (err) {
+        plugins.sass.logError(err);
+        process.exit(1);
+      })
+    );
 });
 
 gulp.task('test_rubysass', function () {
   return plugins
-    // Run all tests in the `tests` folder with Ruby Sass
     .rubySass('./tests')
     .on('error', function (err) {
       console.error('Error!', err.message);
@@ -71,4 +66,4 @@ gulp.task('test', ['test_libsass', 'test_rubysass']);
 // Default task
 // -----------------------------------------------------------------------------
 
-gulp.task('default', ['test', 'concat']);
+gulp.task('default', ['test', 'build']);
