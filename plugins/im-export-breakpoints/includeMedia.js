c280fd1498de8ed5e650ba77b94cb549e09a4412
Eduardo Boucas <mail@eduardoboucas.com>
2015-06-26 16:58:41 +0100

    [WIP] Export breakpoints plugin

---
+++ plugins/im-export-breakpoints/includeMedia.js
@@ -0,0 +1,32 @@
+var includeMedia = (function () {
+  var element = document.body;
+
+  function setElement(newElement) {
+    element = newElement;
+  }
+
+  function readBreakpoints() {
+    if (window.getComputedStyle && (window.getComputedStyle(element).content != "")) {
+      var breakpoints = window.getComputedStyle(element).content.replace(/'/g, '');
+
+      return JSON.parse(breakpoints);
+    }
+
+    return false;
+  }
+
+  function isBreakpointActive(breakpoint) {
+    var breakpoints = readBreakpoints();
+
+    if (!breakpoints) {
+      return false;
+    }
+
+    return breakpoints[breakpoint];
+  }
+
+  return {
+    setElement: setElement,
+    isActive: isBreakpointActive
+  }
+})();
