2d5a4af829d5154befcbcd7449e01ee26e4de15f
Eduardo Bouças <mail@eduardoboucas.com>
2016-11-15 11:00:53 +0000

    Add all SCSS files to test suite

--- tests/index.js
+++ tests/index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const sass = require('node-sass');
 
 sass.render({
-  file: path.resolve(__dirname, 'to-number.scss'),
+  file: path.resolve(__dirname, 'index.scss'),
 }, (err, result) => {
   if (err) throw err
 });
